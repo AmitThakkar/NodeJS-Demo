fabb7b443cbeb7197ed6574bf35700f9370d56b3
amitthakkar <d0ntgetHyper>
2015-05-23 21:17:34 +0530

    correcting the code.

--- Day 1/FileSystemSync.js
+++ Day 1/FileSystemSync.js
@@ -4,8 +4,6 @@
 (function (require) {
     var fs = require('fs');
 
-    fs.unlink('test', function (err) {
-        if (err) throw err;
-        console.log('successfully deleted test');
-    });
+    fs.unlinkSync('test2');
+    console.log('successfully deleted test2');
 })(require);
