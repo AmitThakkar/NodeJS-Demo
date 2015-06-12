96eced9dab84dbc939c677b2557644d5928545a5
amitthakkar <d0ntgetHyper>
2015-06-12 10:01:43 +0530

    Moving request handler below the create server.

--- Day 1/HttpServer.js
+++ Day 1/HttpServer.js
@@ -1,16 +1,16 @@
 /**
  * Created by Amit Thakkar on 16/05/15.
  */
-(function(require) {
+(function (require) {
     var http = require("http");
-    var requestHandler = function (request, response) {
+    var server = http.createServer(requestHandler);
+    server.listen(9999, function () {
+        console.log('Server running at http://localhost:9999/');
+    });
+    function requestHandler(request, response) {
         console.log("Request received.");
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World");
         response.end();
-    };
-    var server = http.createServer(requestHandler);
-    server.listen(9999, function() {
-        console.log('Server running at http://localhost:9999/');
-    });
+    }
 })(require);
